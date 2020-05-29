90aa2b2e57a053f4e5d96464729e8d14af3edae6
gianluca <perronegian@hotmail.com>
2020-05-29 12:18:23 +0800

    app  structure

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Order extends React.Component {
+    render() {
+        return(
+            <div className="order">
+                <p>I'm the order</p>
+            </div>
+        )
+    }
+}
+
+export default Order;
