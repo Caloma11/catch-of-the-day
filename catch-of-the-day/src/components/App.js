508a07e9e08f56e6c609d90e67bbc2aa76dd8ef5
gianluca <perronegian@hotmail.com>
2020-05-30 16:18:50 +0800

    added firebase realtime db

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,12 +4,29 @@ import Inventory from './Inventory';
 import Order from './Order';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base'
 
 class App extends React.Component {
     state = {
         fishes: {},
         order: {}
     }
+
+    componentDidMount() {
+        this.ref = base.syncState(`${this.props.match.params.storeId}/fishes`, {
+            context: this,
+            state: 'fishes'
+        });
+    }
+
+
+    // Done automatically by firebase
+    // componentWillMount() {
+    //     base.removeBinding(this.ref);
+    // }
+
+
+
 
     addFish = (fish) => {
        // Take a copy of the fishes
