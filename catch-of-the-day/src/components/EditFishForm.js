ea0ad9ed9ca07357797ecd53e94fab5d3041d6ff
gianluca <perronegian@hotmail.com>
2020-06-01 11:13:05 +0800

    edit fish form editing state

---
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+
+class EditFishForm extends React.Component {
+
+    handleChange = (e) => {
+        // Take a copy of the current fish
+        const updatedFish = {
+            ...this.props.fish,
+            [e.currentTarget.name]: e.currentTarget.value };
+
+        this.props.editFish(this.props.fishKey, updatedFish);
+    }
+
+
+    render(){
+        const {name, price, status, desc, image} = this.props.fish;
+
+        return(
+            <div className="fish-edit">
+            <input type="text" name="name" onChange={this.handleChange} value={name}/>
+            <input type="text" name="price" onChange={this.handleChange} value={formatPrice(price)} />
+            <select type="text" name="status" onChange={this.handleChange} value={status}>
+                <option onChange={this.handleChange} value="available">Fresh!</option>
+                <option onChange={this.handleChange} value="unavailable">Sold Out</option>
+            </select>
+            <input type="textarea" name="desc" onChange={this.handleChange} value={desc}/>
+            <input type="text" name="image" onChange={this.handleChange} value={image}/>
+
+
+            </div>
+        )
+    }
+}
+
+export default EditFishForm;
