3b8f2753bc41277a1b627d3bd22c7cf32532dc1d
gianluca <perronegian@hotmail.com>
2020-06-05 15:15:53 +0800

    type validations required

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
 
-const Inventory = (props) => {
-    return(
-        <div className="inventory">
-            <h2>Inventory</h2>
-            {Object.keys(props.fishes).map((key) => <EditFishForm key={key} fishKey={key} editFish={props.editFish} fish={props.fishes[key]} deleteFish={props.deleteFish} />)}
-            <AddFishForm addFish={props.addFish} loadSamples={props.loadSamples}/>
-            <button onClick={props.loadSamples}>Load Sample Fishes</button>
-        </div>
-    );
+class Inventory extends React.Component {
+
+    static propTypes = {
+        fishes: PropTypes.object,
+        addFish: PropTypes.func,
+        loadSamples: PropTypes.func,
+        editFish: PropTypes.func,
+        deleteFish: PropTypes.func
+    }
+
+
+    render() {
+        return(
+            <div className="inventory">
+                <h2>Inventory</h2>
+                {Object.keys(this.props.fishes).map((key) => <EditFishForm key={key} fishKey={key} editFish={this.props.editFish} fish={this.props.fishes[key]} deleteFish={this.props.deleteFish} />)}
+                <AddFishForm addFish={this.props.addFish} loadSamples={this.props.loadSamples}/>
+                <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
+            </div>
+        );
+    }
 }
 
 export default Inventory;
